96450cb4058be87b056be30f24804f8940a07a60
wwiiiii <wwiiiii@naver.com>
2016-07-04 22:04:51 +0900

    d

--- android.js
+++ android.js
@@ -3,7 +3,7 @@ var app = http.createServer(handler);
 var io = require('socket.io').listen(app);
 var fs = require('fs');
 
-app.listen(8124);
+app.listen(8124, function () { console.log('start listen');});
 
 function handler(req, res) {
     res.writeHead(200, { 'content-type': 'text/plain' });
