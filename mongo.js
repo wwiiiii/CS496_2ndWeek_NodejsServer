40cde1e5aab7bb1b1be931999c403f446f359f2c
wwiiiii <wwiiiii@naver.com>
2016-07-04 14:43:56 +0900

    first commit
    -

---
+++ mongo.js
@@ -0,0 +1,35 @@
+﻿var server_ip = 'localhost';
+var mongodb = require('mongodb');
+var server = new mongodb.Server(server_ip, 27017, { auto_reconnect: true });
+
+var db = new mongodb.Db('mydb', server);
+db.open(function (err, db) {
+    if (!err) {
+        db.collection('widget', function (err, collection) {
+            if (err) console.log(err);
+            collection.remove(null, { safe: true }, function (err, result) {
+                if (!err) {
+                    console.log('result of remove ' + result);
+                    var widget1 = {
+                        title: 'first widget',
+                        desc: 'this is desc',
+                        prices: 14.99
+                    };
+                    var widget2 = {
+                        title: 'second widget',
+                        desc: 'this is desc2',
+                        prices: 25.99
+                    };
+                    collection.insert(widget1);
+                    collection.insert(widget2, { safe: true }, function (err, result) {
+                        if (err) console.log(err);
+                        else console.log(result);
+                        db.close();
+                    });
+                }
+                else console.log(err);
+            });
+        });
+    }
+    else console.log(err);
+});
