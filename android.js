dc0d7d10c876c15a812fba5b1bfaf289298ccd05
wwiiiii <wwiiiii@naver.com>
2016-07-04 21:53:54 +0900

    d

---
+++ android.js
@@ -0,0 +1,17 @@
+﻿var http = require('http');
+var app = http.createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+
+app.listen(8124);
+
+function handler(req, res) {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.end('Hello from server');
+}
+
+io.sockets.on('connection', function(socket){
+    socket.on('sMsg', function(data){
+        io.sockets.emit('rMsg', data);
+    });
+});
