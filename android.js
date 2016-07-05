08d3306839eee0404b4b57c6f771d3dd1e44b369
wwiiiii <wwiiiii@naver.com>
2016-07-05 15:15:59 +0900

    d

--- android.js
+++ android.js
@@ -7,19 +7,29 @@ var clients = [];
 
 app.listen(8124, function () { console.log('start listen');});
 
+function getIdxBySockID(sockid)
+{
+    for (var i = 0 ; i < clients.length; i++)
+    {
+        if (clients[i].id == sockid) return i;
+    }
+}
+
 function handler(req, res) {
     res.writeHead(200, { 'content-type': 'text/plain' });
     res.end('Hello from server');
 }
 
 io.sockets.on('connection', function (socket) {
+    var clie = new Object();
+    clie.id = socket.id;
+    clients.push(clie);
 
     socket.on('init', function (data) {
         var cli = new Object();
-        cli.fbid = data.fbid;
-        cli.name = data.name;
-        cli.id = socket.id;
-        clients.push(cli);
+        var idx = getIdxBySockID(socket.id);
+        clients[i].fbid = data.fbid;
+        clients[i].name = data.name;
         //io.sockets.connected[cli.id].send('init', data);
         io.to(cli.id).emit('init', data);
         console.log('init');
@@ -29,7 +39,7 @@ io.sockets.on('connection', function (socket) {
     });
 
     socket.on('connect', function () {
-        console.log('disconnet');
+        console.log('connet');
         console.log(socket.id);
     });
 
